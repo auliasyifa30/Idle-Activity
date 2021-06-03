cb276134b763afabfe796ce9f7abac78d09d1712
auliasyifa30 <auliasyifa346@gmail.com>
2021-06-03 23:56:57 +0700

    regis dan login multi user

--- idle_activity/app/Views/admin/index.php
+++
@@ -1,7 +0,0 @@
-<?= $this->extend('templates/index'); ?>
-
-<?= $this->section('page-content'); ?>
-<div class="container-fluid">
-    <h1 class="h3 mb-4 text-gray-800">Welcome to Dashboard Admin</h1>
-</div>
-<?= $this->endSection(); ?>
--- idle_activity/app/Views/user/index.php
+++
@@ -1,7 +0,0 @@
-<?= $this->extend('templates/index'); ?>
-
-<?= $this->section('page-content'); ?>
-<div class="container-fluid">
-    <h1 class="h3 mb-4 text-gray-800">Welcome to Dashboard User</h1>
-</div>
-<?= $this->endSection(); ?>
